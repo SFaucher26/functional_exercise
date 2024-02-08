21aa848ea4b1ce16f9c39c21fa7fee3d142fd9d7
SFaucher26 <sandrine.faucher@maif.fr>
2024-02-08 15:48:45 +0100

    rebase

--- chapter01/exercice04/index.js
+++ chapter01/exercice04/index.js
@@ -14,20 +14,14 @@ export function filterArrayWithCallback(array, filterFunc, callback) {
 
 
 export async function asyncAdd(a, b) {
-
   return new Promise((resolve, reject) => {
-
       setTimeout(() => {
-
-          const result = a + b;
-
+        let result = a + b;
           resolve(result);
-
       }, 1000);
-
   });
-
 }
+
 export async function filterArrayAsync(array, filterFunc) {
   setTimeout(() => {
 
