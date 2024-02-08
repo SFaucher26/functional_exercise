398ff339a21776a15ff978122c2a641533c4e093
SFaucher26 <sandrine.faucher@maif.fr>
2024-02-08 15:48:45 +0100

    modif exercice 3

--- chapter01/exercice01/index.js
+++ chapter01/exercice01/index.js
@@ -1,11 +1,18 @@
 
 export function greetWorld() {
-
+return "Hey, world!";
 }
 
 export function greet(name) {
+    
+    return "Hey, "+name +"!" ;
 }
 
+/**
+ * 
+ * @param {string} name 
+ * @returns 
+ */
 export function capitilizedGreet(name) {
-
+return greet(name.slice(0,1).toUpperCase() + name.slice(1).toLowerCase());
 }
