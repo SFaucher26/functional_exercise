a0eb84057ef18dae60596fe7c3533a0e41797222
SFaucher26 <sandrine.faucher@maif.fr>
2024-02-08 15:48:45 +0100

    commit avant rebase

--- chapter01/exercice03/index.js
+++ chapter01/exercice03/index.js
@@ -1,8 +1,16 @@
 export function reverseString(str) {
+    return str.toString().split('').reverse().join('')
 }
 
 export function isPalindrome(str) {
+    let str2 = str.toString().split('').reverse().join('')
+    if(str == str2){
+        return true 
+    }else{
+        return false
+    }
 }
 
 export function countVowels(str) {
+    return str.match(/[aeiouy]+/gi).length;
 }
